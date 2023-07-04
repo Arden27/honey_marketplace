58bdf8db440de28c2f22040a31f3caf86f511913
Artem Furman <artemfurman@gmail.com>
2023-07-04 03:40:07 +0200

    all components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import './App.css'; // Assuming you have an App.css file for global styles
+
 
 import Header from './components/Header';
 import Home from './components/Home';
@@ -8,6 +10,12 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
       <Header />
       <Home />
       <Footer />
