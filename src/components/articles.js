60c6653547de6e812b617c79bb73fe00fcf52ee5
Artem Furman <artemfurman@gmail.com>
2023-07-07 00:50:23 +0200

    nazwyplików poprawione

---
+++ src/components/articles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Articles() {
+	return (
+		<section className="articles">
+			<article className="articles article-1">ARTYKUŁ1</article>
+			<article className="articles article-2">ARTYKUŁ2</article>
+			<article className="articles article-3">ARTYKUŁ3</article>
+			<article className="articles article-4">ARTYKUŁ4</article>
+		</section>
+	);
+}
