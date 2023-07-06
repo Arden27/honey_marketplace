f3a990a70a840f61e5faa14483b439de022d0953
Artem Furman <artemfurman@gmail.com>
2023-07-06 02:26:50 +0200

    keywords przeniesiony do pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,8 +15,6 @@ export default function RootLayout({ children }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 				<meta name="author" content="Kurpiowski Bartnik" />
-				<meta name="keywords" content="TAGI" />
-				<meta name="description" content="OPIS STRONY" />
 
 				<link
 					rel="stylesheet"
