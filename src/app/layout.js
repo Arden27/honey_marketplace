8edc51dd82a8a72cc75c223b198deabc4ea2cf76
Artem Furman <artemfurman@gmail.com>
2023-07-08 17:03:37 +0200

    accept incom

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,7 @@ import Footer from "../components/footer";
 
 export default function RootLayout({ children }) {
 	return (
-<<<<<<< HEAD
-		<>
-=======
     	<html lang="en">
->>>>>>> 010b4c2 (bug z brakiem html w layout.js naprawiony, header naprawiony (for => htmlFor, class => className)
 			<Head>
 				{/* <meta charSet="UTF-8" />
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
