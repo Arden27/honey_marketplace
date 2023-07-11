566548f72d244b602c4851e67e41435bc8bb4258
Brydak <brydak@outlook.com>
2023-07-11 10:01:19 +0200

    .

--- src/layout/header.js
+++ src/layout/header.js
@@ -36,7 +36,7 @@ export default function Header() {
 						<div className="logo--shadow"></div>
 						
 						<Image
-        					src="/img/test.png"
+        					src="/img/logo.png"
         					width={80}
         					height={80}
       		  				alt="Logo pasieki Kurpiowski Bartnik"
