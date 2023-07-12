c47db1d22c999aeda86e9d32c8f190d7ab2f14df
Brydak <brydak@outlook.com>
2023-07-12 23:49:39 +0200

    shop-menu

--- src/layout/header.js
+++ src/layout/header.js
@@ -63,24 +63,30 @@ export default function Header() {
 
 			<div className="shop-menu-wrapper">
 				<div className="shop-menu">
-					<div className="shop-menu__item">
-						<h2>Promocje</h2>
-					</div>
 
 					<div className="shop-menu__item">
-						<h2>Miody</h2>					
+						<div className="shop-menu__item__img shop-menu__item__img--sale"></div>
+						<h2 className="shop-menu__item__text">Promocje</h2>
 					</div>
 
-					<div className="shop-menu__item">					
-						<h2>Produkty Pszczele</h2>					
+					<div className="shop-menu__item">
+						<div className="shop-menu__item__img shop-menu__item__img--honey"></div>
+						<h2 className="shop-menu__item__text">Miody</h2>					
 					</div>
 
-					<div className="shop-menu__item">					
-						<h2>Świece</h2>			
+					<div className="shop-menu__item">	
+						<div className="shop-menu__item__img shop-menu__item__img--bee-products"></div>				
+						<h2 className="shop-menu__item__text">Produkty Pszczele</h2>					
 					</div>
 
-					<div className="shop-menu__item">					
-						<h2>Zestawy</h2>					
+					<div className="shop-menu__item">	
+						<div className="shop-menu__item__img shop-menu__item__img--candles"></div>				
+						<h2 className="shop-menu__item__text">Świece</h2>			
+					</div>
+
+					<div className="shop-menu__item">
+						<div className="shop-menu__item__img shop-menu__item__img--sets"></div>				
+						<h2 className="shop-menu__item__text">Zestawy</h2>					
 					</div>
 				</div>				
 			</div>
