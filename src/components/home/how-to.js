e2283da997c766424fc2f706501c23b5d4dc1b2f
Brydak <brydak@outlook.com>
2023-07-13 23:31:31 +0200

    .

--- src/components/home/how-to.js
+++ src/components/home/how-to.js
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function HowTo() {
 	return (
-		<section className="how-to" >
-			<div className="slider__img">
+		<section className="how-to">
+			<div className="how-to-wrapper"></div>
+			<div className="how-to__img">
 				<Image
         			src="/img/home/jak-rozpoznac.png"
         			width={1000}
         			height={1000}
       		  		alt="Koszyk"
      			/>
-
 			</div>
 			<div className="how-to__text">
 				<h2>Jak poznać prawdziwy miód?</h2>
