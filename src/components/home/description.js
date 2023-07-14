c9e3ea1216d2d26d2d7de926247420617607c964
Brydak <brydak@outlook.com>
2023-07-15 00:44:26 +0200

    .

--- src/components/home/description.js
+++ src/components/home/description.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Description() {
-	return <article className="description">Miód z Kurpiów</article>;
+	return <article className="text-wrapper text-flow description">
+			<h2 className="description--header">Miód z Kurpiów</h2>
+			<p className="description--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque, temporibus tenetur nostrum eligendi rerum, et deserunt at minus accusantium cum recusandae modi libero sapiente dicta repellat aliquam perspiciatis, commodi ullam laboriosam molestias blanditiis dolor alias sunt voluptates! Perferendis voluptates modi cupiditate id atque dolor asperiores eum molestias tempora, architecto aperiam iste velit iusto dignissimos esse aliquam nulla! Optio consequatur expedita blanditiis, odit corporis quos ipsa fugit, aliquam</p>
+			
+			<div className="description--btn-wrapper">
+				<button className="button description--btn">Czytaj dalej</button>
+			</div>
+		</article>;
 }
