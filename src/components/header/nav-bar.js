6f709f9134fcfc35bbe43798f1ccafc706f36501
Brydak <brydak@outlook.com>
2023-07-16 01:30:18 +0200

    test

--- src/components/header/nav-bar.js
+++ src/components/header/nav-bar.js
@@ -44,24 +44,33 @@ const Nav = () => {
     return (
         <div className="page-wrapper nav-bar-wrapper" >
             <div className="nav-bar" ref={node}>
-                <div className="burger-btn-wrapper">
-                    <button className="button burger-btn" onClick={() => setOpen(!open)}>
-                        <svg
-                            width="25"
-                            height="25"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
-                        </svg>
-                    </button>
-                </div>
+
+                {/* <Link 
+                    className="button" 
+                    href="/"
+                    onClick={() => !isLargeScreen && setOpen(false)}
+                >
+                    <div className="burger-btn-wrapper">
+                        <button className="button burger-btn" onClick={() => setOpen(!open)}>
+                            <svg
+                                width="25"
+                                height="25"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <line x1="3" y1="12" x2="21" y2="12"></line>
+                                <line x1="3" y1="6" x2="21" y2="6"></line>
+                                <line x1="3" y1="18" x2="21" y2="18"></line>
+                            </svg>
+                        </button>
+                    </div>
+                </Link> */}
+
+
 
                 <div className="shop-btn-wrapper">
                     <div className="button shop-btn">Sklep</div>
