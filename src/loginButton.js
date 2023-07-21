d30b01421e57c7616d5ac904316d3b82c6b10ce8
Artem Furman <artemfurman@gmail.com>
2023-07-21 15:59:19 +0200

    logout added

--- src/loginButton.js
+++ src/loginButton.js
@@ -3,13 +3,26 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setIsLoggedIn } from '@/redux/store';
+import { logoutUser } from './api';
 
 function LoginButton() {
     const dispatch = useDispatch();
     const isLoggedIn = useSelector(state => state.isLoggedIn);
 
-    const handleLoginLogout = () => {
-        dispatch(setIsLoggedIn(!isLoggedIn));
+    const handleLoginLogout = async () => {
+        if(isLoggedIn) {
+            try {
+                await logoutUser();
+            } catch (error) {
+                console.log(error);
+            } finally {
+                localStorage.removeItem('token');
+                //dispatch(resetStore());
+                dispatch(setIsLoggedIn(false));
+                //navigate('/login');
+            }
+        }
+        //dispatch(setIsLoggedIn(!isLoggedIn));
     }
 
     return (
