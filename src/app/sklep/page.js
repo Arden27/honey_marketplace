a927effebc799f5414a18b016350b86349d51751
Brydak <brydak@outlook.com>
2023-07-21 16:54:07 +0200

    shop

--- src/app/sklep/page.js
+++ src/app/sklep/page.js
@@ -14,6 +14,10 @@ export default function Shop() {
 	return (
 		<main className="page-wrapper main shop">
 			<div className="shop__1">
+				<div className="shop__panel">
+					<h1 className="shop__panel__category">Sklep / Miody</h1>
+					<button className="button shop__panel__sort-btn">Sortuj</button>
+				</div>
 				<div className="shop__item">
 					<Image
 						className="shop__item__img"
@@ -43,7 +47,7 @@ export default function Shop() {
 						<div className="shop__item__content shop__item__content__price">
 							99,90 zł
 						</div>
-						<button className="button-big shop__item__content shop__item__content__add-to-basket">
+						<button className="button-big shop__item__content shop__item__content__add-to-cart">
 							Dodaj do koszyka
 						</button>
 					</div>
@@ -60,8 +64,8 @@ export default function Shop() {
 
 					<div className="shop__item__content-wrapper">
 						<h2 className="shop__item__content shop__item__content__name">
-							Miód rzepakowy<br />
-							z nutką gryki
+							Miód rzepakowy
+							<br />z nutką gryki
 						</h2>
 
 						<div className="shop__item__content__weight-wrapper">
@@ -79,17 +83,12 @@ export default function Shop() {
 						<div className="shop__item__content shop__item__content__price">
 							99,90 zł
 						</div>
-						<button className="button-big shop__item__content shop__item__content__type">
+						<button className="button-big shop__item__content shop__item__content__add-to-cart">
 							Dodaj do koszyka
 						</button>
 					</div>
 				</div>
 
-
-				<div className="shop__item">1</div>
-				<div className="shop__item">1</div>
-				<div className="shop__item">1</div>
-				<div className="shop__item">1</div>
 				<div className="shop__item">1</div>
 			</div>
 
