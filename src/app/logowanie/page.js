bc4e27d151bc442fd629d7c6d1cf1f8dd6993f7e
Brydak <brydak@outlook.com>
2023-07-31 23:09:21 +0200

    .

--- src/app/logowanie/page.js
+++ src/app/logowanie/page.js
@@ -10,41 +10,41 @@ export default function LogIn() {
   return (
     <main>
       <div className="page-wrapper grid gap-gap md:grid-cols-2">
-        <section className="flex flex-col items-center  overflow-hidden rounded-[3rem] bg-bg2 p-gap">
-          <fieldset>
-            <legend>
-              Zaloguj się
-            </legend>
-            <label for="email">Email</label>
+        <section className="">
+          <form>
+            <fieldset className="flex flex-col  overflow-hidden rounded-[3rem] bg-bg2 p-gap">
+              <legend className="sr-only">Zaloguj się</legend>
+              <h2 className="self-center">Zaloguj się</h2>
+              <label for="email">Email</label>
 
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="Podaj swój email..."
-            />
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Podaj swój email..."
+              />
 
-            <label for="password">Hasło</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Podaj swoje hasło..."
-            />
+              <label for="password">Hasło</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Podaj swoje hasło..."
+              />
 
-            <div>
-              <input type="checkbox" id="remember-me" />
-              <label for="remember-me">Zapamiętaj mnie</label>
+              <div className="flex justify-between">
+                <div>
+                  <input type="checkbox" id="remember-me" />
+                  <label for="remember-me">Zapamiętaj mnie</label>
+                </div>
 
-              <Link  href="/rejestracja">
-                Nie pamiętam hasła
+                <Link href="/rejestracja">Nie pamiętam hasła</Link>
+              </div>
+              <Link className="btn-lg bottom-gap bg-bg hover:text-bg" href="/">
+                Zaloguj się
               </Link>
-            </div>
-          </fieldset>
-
-          <Link className="btn-lg bottom-gap bg-bg hover:text-bg" href="/">
-            Zaloguj się
-          </Link>
+            </fieldset>
+          </form>
         </section>
 
         <section>
