333dbacedeee938528347ce5a34f3b365c9fcec4
Artem Furman <artemfurman@gmail.com>
2023-09-16 13:34:23 +0200

    more products added

--- src/components/load-db.js
+++ src/components/load-db.js
@@ -21,7 +21,7 @@ export default function LoadDB() {
 		{
 			id: 2,
 			name: "Miód rzepakowy\nz nutką gryki",
-			image: "/img/logo.png",
+			image: "/img/products/honey/honey1.jpeg",
 			sizes: [
 				{ size: "250 G", price: 59.99 },
 				{ size: "450 G", price: 79.99 },
@@ -30,32 +30,122 @@ export default function LoadDB() {
 		},
 		{
 			id: 3,
-			name: "Miód rzepakowy\nz nutką gryki",
-			image: "/img/logo.png",
+			name: "Miód firmowy",
+			image: "/img/products/honey/honey2.webp",
 			sizes: [
 				{ size: "250 G", price: 59.99 },
-				{ size: "450 G", price: 79.99 },
+				{ size: "450 G", price: 79.99, default: true },
 				{ size: "1 KG", price: 99.99 },
 			],
 		},
         {
 			id: 4,
-			name: "Aiód rzepakowy\nz nutką gryki",
-			image: "/img/logo.png",
+			name: "Miod gorzki",
+			image: "/img/products/honey/honey3.webp",
 			sizes: [
-				{ size: "250 G", price: 2359.99 },
-				{ size: "450 G", price: 79.99 },
-				{ size: "1 KG", price: 99.99 },
+				{ size: "250 G", price: 49.99 },
+				{ size: "450 G", price: 59.99, default: true },
+				{ size: "1 KG", price: 69.99 },
 			],
 		},
         {
 			id: 5,
-			name: "ZAiód rzepakowy\nz nutką gryki",
-			image: "/img/logo.png",
+			name: "Miod z krowy",
+			image: "/img/products/honey/honey4.jpeg",
 			sizes: [
 				{ size: "250 G", price: 19.99 },
-				{ size: "450 G", price: 79.99 },
-				{ size: "1 KG", price: 99.99 },
+				{ size: "450 G", price: 29.99, default: true },
+				{ size: "1 KG", price: 39.99 },
+			],
+		},
+        {
+			id: 6,
+			name: "Bzestaw miodu",
+			image: "/img/products/sets/zestaw_miodu5.jpeg",
+			sizes: [
+				{ size: "450 G", price: 769.99 },
+				{ size: "1 KG", price: 1089.99, default: true },
+			],
+		},
+        {
+			id: 7,
+			name: "Zestaw miodu powiększony",
+			image: "/img/products/sets/zestaw_miodu2.jpeg",
+			sizes: [
+				{ size: "250 G", price: 489.99 },
+				{ size: "450 G", price: 869.99, default: true },
+				{ size: "1 KG", price: 989.99 },
+			],
+		},
+        {
+			id: 8,
+			name: "Zestaw miodu",
+			image: "/img/products/sets/zestaw_miodu3.jpeg",
+			sizes: [
+                { size: "250 G", price: 389.99 },
+				{ size: "450 G", price: 669.99, default: true },
+				{ size: "1 KG", price: 889.99 },
+			],
+		},
+        {
+			id: 9,
+			name: "Elitny zestaw miodu",
+			image: "/img/products/sets/zestaw_miodu4.png",
+			sizes: [
+				{ size: "3 KG", price: 1069.99, default: true },
+				{ size: "5 KG", price: 1489.99 },
+			],
+		},
+        {
+			id: 10,
+			name: "Świca oryginalna",
+			image: "/img/products/candles/candle1.webp",
+			sizes: [
+				{ size: "3 KG", price: 15.99, default: true }
+			],
+		},
+        {
+			id: 11,
+			name: "Świca czarna",
+			image: "/img/products/candles/candle2.jpeg",
+			sizes: [
+				{ size: "3 KG", price: 21.99, default: true }
+			],
+		},
+        {
+			id: 12,
+			name: "Świca prosto z miodu",
+			image: "/img/products/candles/candle4.jpeg",
+			sizes: [
+				{ size: "3 KG", price: 16.99, default: true }
+			],
+		},
+        {
+			id: 13,
+			name: "Świca podstawowa",
+			image: "/img/products/candles/candle3.jpeg",
+			sizes: [
+				{ size: "3 KG", price: 12.99, default: true }
+			],
+		},
+        {
+			id: 14,
+			name: "Zestaw świec miodowych",
+			image: "/img/products/sets/zestaw_swiec1.jpeg",
+			sizes: [
+				{ size: "10 szt", price: 35.99 },
+				{ size: "25 szt", price: 50.99, default: true },
+				{ size: "40 szt", price: 99.99 },
+			],
+		},
+        {
+			id: 15,
+			name: "Mega miodowy zestaw świec",
+			image: "/img/products/sets/zestaw_swiec2.jpeg",
+			sizes: [
+				{ size: "50 szt", price: 139.99 },
+				{ size: "85 szt", price: 249.99, default: true },
+				{ size: "130 szt", price: 599.99 },
 			],
 		},
 		// add more items as needed
