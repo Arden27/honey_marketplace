d6ddd8ad937d5cc4bb0f72a141ed32876a9c436f
Brydak <brydak@outlook.com>
2023-10-29 00:04:16 +0200

    .

--- src/components/page-wrapper.js
+++ src/components/page-wrapper.js
@@ -1,4 +1,4 @@
-export default function PageWrapper({ className, children }) {
+export default function PageWrapper({ children }) {
   return (
     <div className="mx-auto mb-sm w-[calc(100%-2*theme(spacing.sm))] max-w-screen-xl">
       {children}
