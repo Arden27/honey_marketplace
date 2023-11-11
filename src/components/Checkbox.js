76b4067e4d6c9919bd88370629eb28b75fe9949b
Brydak <brydak@outlook.com>
2023-11-11 22:36:53 +0100

    style: checkbox positioning

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,13 +1,13 @@
 export default function Checkbox({ className, children, ...props }) {
   return (
     <label
-      className={` mb-3xs  flex
+      className={` mb-3xs flex 
       ${className}
       `}
       {...props}
     >
       <input
-        className="relative h-sm w-sm flex-none appearance-none rounded-[2rem] border-2 border-text checked:after:absolute checked:after:-top-[9px] checked:after:left-[3px] checked:after:content-['\2714'] "
+        className="relative top-[1px] h-sm w-sm flex-none appearance-none rounded-[2rem] border-2 border-text checked:after:absolute checked:after:-top-[9px] checked:after:left-[3px] checked:after:content-['\2714']  "
         type="checkbox"
       ></input>
       <div className="relative ml-2xs text-sm text-text after:absolute  after:-bottom-2xs after:text-lg after:text-warning after:content-['*']">
