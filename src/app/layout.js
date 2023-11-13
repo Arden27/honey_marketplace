7e282b55a163138a8ae787a4de26142e8a896cfd
Brydak <brydak@outlook.com>
2023-11-13 19:44:44 +0100

    fix: layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pl">
       <body className="bg-bg">
-        <div className="grid grid-rows-[auto-fit_1fr_auto] gap-y-lg [&>*>*]:col-start-2  [&>*>*]:col-end-3 [&>*]:grid [&>*]:grid-cols-[minmax(theme(spacing.2xs),auto)_minmax(auto,1200px)_minmax(theme(spacing.2xs),auto)] [&>*]:gap-y-sm sm:[&>*]:grid-cols-[minmax(theme(spacing.sm),auto)_minmax(auto,1200px)_minmax(theme(spacing.sm),auto)]">
+        <div className="grid grid-rows-[auto-fit_1fr_auto] gap-y-lg [&>*>*]:col-start-2  [&>*>*]:col-end-3 [&>*]:grid [&>*]:grid-cols-[minmax(theme(spacing.2xs),auto)_minmax(auto,theme(screens.xl))_minmax(theme(spacing.2xs),auto)] [&>*]:gap-y-sm sm:[&>*]:grid-cols-[minmax(theme(spacing.sm),auto)_minmax(auto,theme(screens.xl))_minmax(theme(spacing.sm),auto)]">
           <Providers>
             <LoadDB />
             <Header />
--- src/app/sklep/page.js
+++ src/app/sklep/page.js
@@ -73,7 +73,7 @@ export default function Shop() {
               </DropdownMenu.List>
             </DropdownMenu>
           </div>
-          <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] grid-rows-2 gap-sm">
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-sm">
             <Items sortType={sortType} category={category} />
           </div>
 
